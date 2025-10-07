5725cda0699389675cbbc5d89953cca95d8f1112
Alejo <alejodm12345@gmail.com>
2025-10-07 08:53:48 -0300

    Entidad usuario

--- src/modules/usuario/entities/usuario.entity.ts
+++ src/modules/usuario/entities/usuario.entity.ts
@@ -1,1 +1,22 @@
-export class Usuario {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('usuarios')
+export class Usuario {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  nombre: string;
+
+  @Column()
+  apellido: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  fechaHoraCreacion: Date;
+}
