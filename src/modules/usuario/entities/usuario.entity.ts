1a693da4f91ab60e1da9847001c9dafdb2a729cc
Alejo <alejodm12345@gmail.com>
2025-10-07 09:00:17 -0300

    Especificaciones Swagger en entidad Usuario

--- src/modules/usuario/entities/usuario.entity.ts
+++ src/modules/usuario/entities/usuario.entity.ts
@@ -1,22 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('usuarios')
 export class Usuario {
+  @ApiProperty({ example: 1, description: 'Identificador único del usuario' })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({ example: 'Alejo', description: 'Nombre del usuario' })
   @Column()
   nombre: string;
 
+  @ApiProperty({ example: 'De Miguel', description: 'Apellido del usuario' })
   @Column()
   apellido: string;
 
+  @ApiProperty({ example: 'alejo@gmail.com', description: 'Email del usuario' })
   @Column({ unique: true })
   email: string;
 
+  @ApiProperty({
+    example: 'password123',
+    description: 'Contraseña del usuario',
+  })
   @Column()
   password: string;
 
+  @ApiProperty({
+    example: '2023-10-01T12:00:00Z',
+    description: 'Fecha y hora de creación del usuario',
+  })
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   fechaHoraCreacion: Date;
 }
