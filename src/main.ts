e8142569d63ef19fd79fdc1f169d2ea1408b6449
Alejo <alejodm12345@gmail.com>
2025-10-07 08:56:05 -0300

    Configuración Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule,
+  DocumentBuilder
+} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API de Ventas')
+    .setDescription('Documentación de la API del proyecto de ventas')
+    .setVersion('1.0')
+    .addTag('ventas')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
