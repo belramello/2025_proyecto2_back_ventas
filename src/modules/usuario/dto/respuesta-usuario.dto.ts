c0347722d8f2f1f04b46a07256c7c3a70d022bba
Alejo <alejodm12345@gmail.com>
2025-10-11 14:07:18 -0300

    DTO de RespuestaUsuario

---
+++ src/modules/usuario/dto/respuesta-usuario.dto.ts
@@ -0,0 +1,54 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class RespuestaUsuarioDto {
+  @ApiProperty({
+    example: 1,
+    description: 'Identificador único del usuario',
+    type: Number,
+    required: true,
+  })
+  id: number;
+
+  @ApiProperty({
+    example: 'Alejo',
+    description: 'Nombre del usuario',
+    type: String,
+    required: true,
+  })
+  nombre: string;
+
+  @ApiProperty({
+    example: 'De Miguel',
+    description: 'Apellido del usuario',
+    type: String,
+    required: true,
+  })
+  apellido: string;
+
+  @ApiProperty({
+    example: 'alejodm12345@gmail.com',
+    description: 'Email único del usuario',
+    type: String,
+    required: true,
+  })
+  email: string;
+
+  @ApiProperty({
+    example: '2023-10-01T12:00:00Z',
+    description: 'Fecha y hora de creación del usuario',
+    type: String,
+    format: 'date-time',
+    required: true,
+  })
+  fechaHoraCreacion: Date;
+
+  /*
+  @ApiProperty({
+    example: 'Auditor',
+    description: 'Rol del usuario',
+    type: Rol,
+    required: true,
+  })
+  rol: Rol;
+  */
+}
