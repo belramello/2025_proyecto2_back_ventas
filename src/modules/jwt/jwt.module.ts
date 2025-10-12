3793e48b7c967240225f559fdd473ac18190ac04
Alejo <alejodm12345@gmail.com>
2025-10-12 18:07:34 -0300

    Eliminación controlador jwt

--- src/modules/jwt/jwt.module.ts
+++ src/modules/jwt/jwt.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtService } from './jwt.service';
-import { JwtController } from './jwt.controller';
 
 @Module({
-  controllers: [JwtController],
   providers: [JwtService],
   exports: [JwtService],
 })
