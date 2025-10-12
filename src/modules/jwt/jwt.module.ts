26dd0ad6bbf1eec62203d171ed2bc7eb7e796695
Alejo <alejodm12345@gmail.com>
2025-10-12 18:18:35 -0300

    Configuración módulo JWT

--- src/modules/jwt/jwt.module.ts
+++ src/modules/jwt/jwt.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { JwtModule as NestJwtModule } from '@nestjs/jwt';
 import { JwtService } from './jwt.service';
 
 @Module({
+  imports: [
+    NestJwtModule.register({
+      secret: process.env.JWT_AUTH_SECRET || 'authSecret',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   providers: [JwtService],
   exports: [JwtService],
 })
--- src/modules/jwt/jwt.service.ts
+++ src/modules/jwt/jwt.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class JwtService {}
+export class JwtService {
+  config = {
+    auth: {
+      secret: 'authSecret', // Clave secreta para los tokens de autenticacion
+      expiresIn: '1d', // Tiempo de expiración para los tokens de autenticación
+    },
+    refresh: {
+      secret: 'refreshSecret', // Clave secreta para los tokens de refresh
+      expiresIn: '1d', // Tiempo de expiración para los tokens de refresh
+    },
+  };
+}
