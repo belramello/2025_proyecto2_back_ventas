50c3034db564ddfa54cd6753211cefc89d5a9431
MartinBeccereca <martinbeccereca@gmail.com>
2025-10-15 10:36:12 -0300

    feat(paginacion): Crear mapper

---
+++ src/modules/productos/mapper/producto.mapper.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@nestjs/common';
+import { Producto } from '../entities/producto.entity';
+import { RespuestaCreateProductoDto } from '../dto/respuesta-create-producto.dto';
+import { RespuestaFindOneProductoDto } from '../dto/respuesta-find-one-producto.dto';
+import { RespuestaFindAllPaginatedProductoDTO } from '../dto/respuesta-find-all-paginated.dto';
+
+@Injectable()
+export class ProductoMapper {
+
+  toRespuestaCreateProducto(producto: Producto): RespuestaCreateProductoDto {
+    return {
+      nombre: producto.nombre,
+      codigo: producto.codigo,
+    precio: producto.precio,
+    marca: producto.marca,
+    stock: producto.stock,
+    linea: producto.linea,
+    fotoUrl: producto.fotoUrl,
+    descripcion: producto.descripcion
+
+    };
+  }
+
+  toRespuestaFinalFindOneDto(producto: Producto): RespuestaFindOneProductoDto{
+    return {
+      id: producto.id,
+      nombre: producto.nombre,
+      codigo: producto.codigo,
+      precio: producto.precio,
+      marca: producto.marca,
+      stock: producto.stock,
+      linea: producto.linea,
+      fotoUrl: producto.fotoUrl,
+      fechaCreacion: producto.fechaCreacion,
+      descripcion: producto.descripcion};
+  }
+
+  toRespuestaFindAllProductosDTO(productos: Producto[]): RespuestaFindOneProductoDto[] {
+    return productos.map((producto) => this.toRespuestaFinalFindOneDto(producto));
+  }
+
+  toRespuestaFindAllPaginatedVentaDTO(paginated: {
+    productos: Producto[];
+    total: number;
+    page: number;
+    lastPage: number;
+  }): RespuestaFindAllPaginatedProductoDTO{
+    return {
+      productos: this.toRespuestaFindAllProductosDTO(paginated.productos),
+      total: paginated.total,
+      page: paginated.page,
+      lastPage: paginated.lastPage,
+    };
+  }
+}
