28ecea2a76cc4953df115b6287f38a3db897a96b
Alejo <alejodm12345@gmail.com>
2025-10-20 18:46:22 -0300

    Formatear a EsLint

--- src/modules/marcas/marcas.controller.ts
+++ src/modules/marcas/marcas.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
 import { MarcasService } from './marcas.service';
 import { CreateMarcaDto } from './dto/create-marca.dto';
 import { UpdateMarcaDto } from './dto/update-marca.dto';
