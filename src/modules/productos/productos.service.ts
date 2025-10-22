b6b36cf806dee134bc4315b68ffb8f3fe46e4d32
MartinBeccereca <martinbeccereca@gmail.com>
2025-10-22 10:34:51 -0300

    feat(historial): Agrega historial en actualizar producto

--- src/modules/productos/productos.service.ts
+++ src/modules/productos/productos.service.ts
@@ -73,7 +73,31 @@ export class ProductosService {
     updateProductoDto: UpdateProductoDto,
     usuarioId: number,
   ) {
-    return this.productosRepository.update(id, updateProductoDto, usuarioId);
+    try {
+      const producto = this.productosRepository.update(
+        id,
+        updateProductoDto,
+        usuarioId,
+      );
+
+      // Registro actualización exitosa
+      await this.historialActividades.create({
+        usuario: usuarioId,
+        accionId: 8, // Acción de borrado de producto
+        estadoId: 1, // Exitoso
+      });
+
+      return producto;
+    } catch (error) {
+      // Registro historial fallido
+      await this.historialActividades.create({
+        usuario: usuarioId,
+        accionId: 8,
+        estadoId: 2, // Fallido
+      });
+
+      throw error; // Opcional: volver a lanzar el error
+    }
   }
 
   async decrementarStock(producto: Producto, cantidad: number) {
