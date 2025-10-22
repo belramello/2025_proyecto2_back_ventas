6ff62d53cd6513f49d98258a7d127e3026125ebe
Juan Saby <sabyjuanpablo2004@gmail.com>
2025-10-22 00:21:33 -0300

    aplicación de metodos en el marca-repository, falta corregir algunas cosas

--- src/modules/marcas/marcas.module.ts
+++ src/modules/marcas/marcas.module.ts
@@ -1,14 +1,30 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { MarcasService } from './marcas.service';
 import { MarcasController } from './marcas.controller';
 import { Marca } from './entities/marca.entity';
 import { MarcaRepository } from './repositories/marca-repository';
+import { IMarcaRepository } from './repositories/marca-repository.interface';
+import { JwtModule } from '../jwt/jwt.module';
+import { UsuarioModule } from '../usuario/usuario.module';
 import { MarcaNombreUniqueValidator } from './helpers/marcas-validator';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Marca])],
+  imports: [
+    TypeOrmModule.forFeature([Marca]),
+    JwtModule,
+    UsuarioModule,
+  ],
   controllers: [MarcasController],
-  providers: [MarcasService, MarcaRepository, MarcaNombreUniqueValidator],
+  providers: [
+    MarcasService,
+    {
+      provide: 'IMarcaRepository',
+      useClass: MarcaRepository,
+    },
+    MarcaNombreUniqueValidator,
+  ],
+  exports: [MarcasService],
 })
 export class MarcasModule {}
--- src/modules/marcas/marcas.service.ts
+++ src/modules/marcas/marcas.service.ts
@@ -1,61 +1,51 @@
-import { Injectable, ConflictException, BadRequestException } from '@nestjs/common';
-import { MarcaRepository } from './repositories/marca-repository';
+import { Injectable, Inject, BadRequestException } from '@nestjs/common';
 import { CreateMarcaDto } from './dto/create-marca.dto';
 import { UpdateMarcaDto } from './dto/update-marca.dto';
+import type { IMarcaRepository } from './repositories/marca-repository.interface';
 import { Marca } from './entities/marca.entity';
 
 @Injectable()
 export class MarcasService {
-  constructor(private readonly marcaRepository: MarcaRepository) {}
+  constructor(
+    @Inject('IMarcaRepository')
+    private readonly marcaRepository: IMarcaRepository,
+  ) {}
 
   async create(createMarcaDto: CreateMarcaDto, logoPath?: string): Promise<Marca> {
-    const marca = this.marcaRepository.create({ ...createMarcaDto, logo: logoPath });
-    return this.marcaRepository.save(marca);
+    const marcaData = { ...createMarcaDto, logo: logoPath };
+    return this.marcaRepository.create(marcaData);
   }
 
   async findAll(): Promise<Marca[]> {
-    return this.marcaRepository.find();
+    return this.marcaRepository.findAll();
   }
 
   async findOne(id: number): Promise<Marca> {
-    const marca = await this.marcaRepository.findOneBy({ id });
-    if (!marca) throw new BadRequestException('Marca no encontrada');
-    return marca;
+    const _marca = await this.marcaRepository.findOne(id);
+    if (!_marca) throw new BadRequestException('Marca no encontrada');
+    return _marca;
   }
 
   async update(id: number, updateMarcaDto: UpdateMarcaDto, logoPath?: string): Promise<Marca> {
-    const marca = await this.findOne(id);
-    if (updateMarcaDto.nombre && updateMarcaDto.nombre !== marca.nombre) {
+    const _marca = await this.findOne(id);
+    if (updateMarcaDto.nombre && updateMarcaDto.nombre !== _marca.nombre) {
       const existing = await this.marcaRepository.findByNombre(updateMarcaDto.nombre);
-      if (existing) throw new ConflictException('Nombre ya registrado');
+      if (existing) throw new BadRequestException('Nombre ya registrado');
     }
     if (logoPath) updateMarcaDto['logo'] = logoPath;
     await this.marcaRepository.update(id, updateMarcaDto);
     return this.findOne(id);
   }
-  async remove(id: number): Promise<void> {
-    const marca = await this.findOne(id);
 
-    //lógica para chequear productos asociados cuando lo pase belu
-    await this.marcaRepository.softRemove(marca);
+  async remove(id: number): Promise<void> {
+    const _marca = await this.findOne(id);
+    // TODO: Chequeo de productos asociados cuando lineas esté listo
+    await this.marcaRepository.remove(id);
   }
 
-
-   //Restaura una marca que fue borrada lógicamente.
   async restore(id: number): Promise<void> {
-    const marca = await this.marcaRepository.findOne({ // Buscamos en la base de datos, incluyendo los registros borrados lógicamente
-      where: { id },
-      withDeleted: true,
-    });
-
-    if (!marca) {
-      throw new BadRequestException('Marca no encontrada');
-    }
-
-    if (!marca.deletedAt) {
-      throw new BadRequestException('La marca no se encuentra eliminada');
-    }
-
+    const _marca = await this.marcaRepository.findOne(id);
+    if (!_marca?.deletedAt) throw new BadRequestException('La marca no se encuentra eliminada');
     await this.marcaRepository.restore(id);
   }
 }
