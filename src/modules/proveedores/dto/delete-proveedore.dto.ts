89ade909c96cfcff1b5a1961184ba256a96c850d
Alejo <alejodm12345@gmail.com>
2025-10-26 18:42:12 -0300

    Correcciones formato EsLint

--- src/modules/proveedores/dto/delete-proveedore.dto.ts
+++ src/modules/proveedores/dto/delete-proveedore.dto.ts
@@ -2,7 +2,10 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, Min } from 'class-validator';
 
 export class DeleteProveedoreDto {
-  @ApiProperty({ example: 5, description: 'ID del proveedor a eliminar (soft delete)' })
+  @ApiProperty({
+    example: 5,
+    description: 'ID del proveedor a eliminar (soft delete)',
+  })
   @IsInt()
   @Min(1)
   id: number;
