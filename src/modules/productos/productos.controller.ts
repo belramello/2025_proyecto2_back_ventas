52411e869492606b1dabf132357d2e400034f000
MartinBeccereca <martinbeccereca@gmail.com>
2025-10-27 15:34:55 -0300

    feat(productos): Cambio en controller y dto para poder crear productos correctamente con imagen asociada

--- src/modules/productos/productos.controller.ts
+++ src/modules/productos/productos.controller.ts
@@ -13,6 +13,9 @@ import {
   Req,
   UseInterceptors,
   UploadedFile,
+  NotFoundException,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ProductosService } from './productos.service';
@@ -67,6 +70,14 @@ export class ProductosController {
   @Post()
   @ApiOperation({ summary: 'Crear un nuevo producto' })
   @UseInterceptors(FileInterceptor('imagen', multerOptions))
+  @UsePipes(
+    new ValidationPipe({
+      whitelist: false,
+      forbidNonWhitelisted: false,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  )
   @ApiResponse({
     status: 201,
     description: 'Producto creado correctamente',
